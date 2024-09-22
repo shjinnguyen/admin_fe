9f11a9cb57b80065cdcccd5a9f8037dc5cdcfee9
Felix Nguyen <nguyenvinhphuc.vn@smilegate.com>
2024-09-22 07:59:54 +0700

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import { Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Tables from "./pages/Tables";
-import Billing from "./pages/Billing";
-import Rtl from "./pages/Rtl";
-import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
 import Unit from "./pages/unit/Unit";
 
 import Main from "./components/layout/Main";
@@ -15,25 +8,17 @@ import Sku from "./pages/sku/Sku";
 import Supplier from "./pages/supplier/Supplier";
 import Product from "./pages/product/Product";
 import Inventory from "./pages/inventory/Inventory";
-import ImportPage from "./pages/inventory/Import";
+import InventoryForm from "./pages/inventory/ImportV2";
 
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
-import InventoryForm from "./pages/inventory/ImportV2";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/sign-up" exact component={SignUp} />
-        <Route path="/sign-in" exact component={SignIn} />
         <Main>
-          <Route exact path="/dashboard" component={Home} />
-          <Route exact path="/tables" component={Tables} />
-          <Route exact path="/billing" component={Billing} />
-          <Route exact path="/rtl" component={Rtl} />
-          <Route exact path="/profile" component={Profile} />
           <Route exact path="/unit" component={Unit} />
           <Route exact path="/category" component={Category} />
           <Route exact path="/sku" component={Sku} />
